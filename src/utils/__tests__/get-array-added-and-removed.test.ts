7bff89e3d1380a045af9f24da7f01ec1fce3b21f
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2020-03-06 13:28:46 -0800

    feat: added array differ to see added and removed items

---
+++ src/utils/__tests__/get-array-added-and-removed.test.ts
@@ -0,0 +1,75 @@
+import { getArrayAddedAndRemoved } from "../get-array-added-and-removed";
+
+describe("Get array added and removed", () => {
+	test("to return nothing when they're both the same in the  same order", async () => {
+		const { added, removed } = getArrayAddedAndRemoved(
+			[1, 2, 3, 4],
+			[1, 2, 3, 4]
+		);
+		expect(added.length).toBe(0);
+		expect(removed.length).toBe(0);
+	});
+
+	test("to return nothing when they're both the same in a different order", async () => {
+		const { added, removed } = getArrayAddedAndRemoved(
+			[1, 3, 2, 4],
+			[1, 2, 3, 4]
+		);
+		expect(added.length).toBe(0);
+		expect(removed.length).toBe(0);
+	});
+
+	test("to return an added item when in a same order", async () => {
+		const { added, removed } = getArrayAddedAndRemoved(
+			[1, 2, 3, 4],
+			[1, 2, 3, 4, 5]
+		);
+		expect(added).toEqual([5]);
+		expect(removed.length).toBe(0);
+	});
+
+	test("to return an added item when in a different order", async () => {
+		const { added, removed } = getArrayAddedAndRemoved(
+			[1, 3, 2, 4],
+			[1, 2, 3, 4, 5]
+		);
+		expect(added).toEqual([5]);
+		expect(removed.length).toBe(0);
+	});
+
+	test("to return an removed item when in a same order", async () => {
+		const { added, removed } = getArrayAddedAndRemoved(
+			[1, 2, 3, 4, 5],
+			[1, 2, 3, 4]
+		);
+		expect(removed).toEqual([5]);
+		expect(added.length).toBe(0);
+	});
+
+	test("to return an removed item when in a different order", async () => {
+		const { added, removed } = getArrayAddedAndRemoved(
+			[1, 3, 2, 4, 5],
+			[1, 2, 3, 4]
+		);
+		expect(removed).toEqual([5]);
+		expect(added.length).toBe(0);
+	});
+
+	test("to return an removed and added item when in the same order", async () => {
+		const { added, removed } = getArrayAddedAndRemoved(
+			[1, 2, 3, 4, 5],
+			[1, 2, 3, 4, 6]
+		);
+		expect(removed).toEqual([5]);
+		expect(added).toEqual([6]);
+	});
+
+	test("to return an removed and added item when in a different order", async () => {
+		const { added, removed } = getArrayAddedAndRemoved(
+			[1, 2, 3, 4, 5],
+			[6, 1, 2, 3, 4]
+		);
+		expect(removed).toEqual([5]);
+		expect(added).toEqual([6]);
+	});
+});
